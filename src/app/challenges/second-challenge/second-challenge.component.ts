c64d88f7e00fa628d5230409456117ac4f8f90a3
Bruno Campos <campos.bruno1996@gmail.com>
2022-06-01 13:22:21 -0300

    iniciando desafio 2

--- src/app/challenges/second-challenge/second-challenge.component.ts
+++ src/app/challenges/second-challenge/second-challenge.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-second-challenge',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondChallengeComponent implements OnInit {
 
+  challengeResult!: string;
+
+  secondChallengeForm = new FormGroup({
+    firstString: new FormControl('', Validators.required),
+    secondString: new FormControl('', Validators.required),
+  });
+
+  get isFormValid(): boolean {
+    return this.secondChallengeForm.valid;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  submit() {}
 
 }
